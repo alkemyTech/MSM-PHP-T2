ba29210aec68f840ea1277aa6b4814ebf41f87fd
ana rusconi <anarusconi3@gmail.com>
2023-11-09 16:26:48 -0300

    modificado el modelo

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,6 +9,6 @@ class Role extends Model
 {
     use HasFactory;
     public $timestamps = true;
-    protected $fillable = ['id', 'name', 'descripcion'];
+    protected $fillable = ['id', 'name', 'descrition'];
     protected $hidden = ['created_at', 'updated_at'];
 }

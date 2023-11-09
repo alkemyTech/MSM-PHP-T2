b06440da97f708b009fdc0b86d21b8b50fddb5a1
MLauzan <mateolauzan@gmail.com>
2023-11-08 22:51:49 -0300

    Register Endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+
+{
+    public function register(Request $req)
+    {
+        try {
+            $req->validate([
+                'name' => 'required|string|max:255',
+                'last_name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users,email',
+                'password' => 'required|string|min:6',
+            ]);
+
+            $user = new User();
+            $user->name = $req->name;
+            $user->last_name = $req->last_name;
+            $user->email = $req->email;
+            $user->password = $req['password'] = Hash::make($req['password']);
+            $user->role_id = 2;
+
+            // Crear cuentas ARS y USD
+            $this->createAccount($user, 'ARS', 300000);
+            $this->createAccount($user, 'USD', 1000);
+
+            // Generar el token de acceso
+            $token = $user->createToken('token')->accessToken;
+            return response()->created(['token' => $token, 'user' => $user]);
+        } catch (QueryException $e) {
+            return response()->internalServerError(['message' => $e->errorInfo[2]]); // usa el provider de internalServerError para devolver si hay un error de base de datos y accede al mensaje de error
+        }
+    }
+    protected function createAccount($user, $currency, $limit) // como creamos 2 cuentas usamos una sola función donde se pasa como parámetro el usuario para obtener el id, el tipo de moneda y el límite de transacción
+    {
+        $faker = \Faker\Factory::create();
+
+        do { // genera un cbu aleatorio
+            $cbu = '';
+            for ($i = 0; $i < 22; $i++) {
+                $cbu .= $faker->randomNumber(1, true); // va agregando un número aleatorio al cbu
+            }
+            $cbuUsed = Account::where('cbu', $cbu)->first(); // busca si el cbu existe en la base de datos
+        } while ($cbuUsed); // si encuentra el cbu va a reiniciar el ciclo
+
+        $user->save(); // el usuario se crea acá para evitar que se genere un usuario sin cbu si ocurren problemas
+
+        $account = new Account();
+        $account->currency = $currency;
+        $account->transaction_limit = $limit;
+        $account->balance = 0;
+        $account->user_id = $user->id;
+        $account->cbu = $cbu;
+        $account->save();
+    }
+}

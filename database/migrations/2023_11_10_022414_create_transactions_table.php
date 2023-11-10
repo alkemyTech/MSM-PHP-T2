7eb1776651b1b684d2a7cadc00e46e44d5dd798c
alenmonti <amonti@fi.uba.ar>
2023-11-10 00:36:52 -0300

    document transaction migrate

--- database/migrations/2023_11_10_022414_create_transactions_table.php
+++ database/migrations/2023_11_10_022414_create_transactions_table.php
@@ -11,16 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('transactions', function (Blueprint $table) {
-            $table->id();
-            $table->double('amount');
-            $table->enum('type',['INCOME','PAYMENT','DEPOSIT']);
-            $table->string('description')->nullable();
-            $table->foreignId('account_id')->references('id')->on('accounts');
-            $table->timestamp('transaction_date')->useCurrent();
+        Schema::create('transactions', function (Blueprint $table) { // tabla transactions para los datos de las transacciones de las cuentas de los usuarios
+            $table->id(); // ID de la transacción
+            $table->double('amount'); // Monto de la transacción
+            $table->enum('type',['INCOME','PAYMENT','DEPOSIT']); // Tipo de transacción: INCOME (ingreso), PAYMENT (pago), DEPOSIT (depósito)
+            $table->string('description')->nullable(); // Descripción de la transacción (opcional)
+            $table->foreignId('account_id')->references('id')->on('accounts'); // ID de la cuenta a la que pertenece la transacción
+            $table->timestamp('transaction_date')->useCurrent(); // Fecha y hora de la transacción (por defecto, la fecha y hora actual)
         });
     }
-
     /**
      * Reverse the migrations.
      */

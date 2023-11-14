5ab19fb2dca4a1fca7a8db418f4111db7d5acc98
MLauzan <mateolauzan@gmail.com>
2023-11-14 19:28:05 -0300

    branch corrections

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -49,20 +49,7 @@ class AuthController extends Controller
             return response()->internalServerError(['message' => $e->errorInfo[2]]); // usa el provider de internalServerError para devolver si hay un error de base de datos y accede al mensaje de error
         }
     }
-    public function login(Request $req)
-    {
-        $credentials = $req->validate([
-            'email' => 'required|exists:usuarios,email',
-            'password' => 'required'
-        ]);
-        if (Auth::attempt($credentials)) {
-            $user = User::find(Auth::user()->id);
-            $token = $user->createToken('token')->accessToken;
-            return response()->ok(['Authentication Token' => $token, 'Logged-in user' => $user]);
-        }
-        return response()->unauthorized();
-    }
-}
+
     protected function createAccount($user, $currency, $limit) // como creamos 2 cuentas usamos una sola función donde se pasa como parámetro el usuario para obtener el id, el tipo de moneda y el límite de transacción
     {
         $faker = \Faker\Factory::create();
@@ -82,7 +69,7 @@ class AuthController extends Controller
         $account->cbu = $cbu;
         $account->save();
     }
-  
+
     public function login(Request $req)
     {
         $credentials = $req->validate([
@@ -94,7 +81,6 @@ class AuthController extends Controller
             $token = $user->createToken('token')->accessToken;
             return response()->created(['token' => $token, 'user' => $user]);
         }
-        return response()->unauthorized(['message' => 'Credenciales inválidas']);
+        return response()->unauthorized(['message' => 'Invalid credentials']);
     }
 }
-
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,7 +9,6 @@ class Role extends Model
 {
     use HasFactory;
 
-    public $timestamps = true;
     protected $fillable = ['name', 'description'];
     protected $hidden = ['created_at', 'updated_at'];
 }

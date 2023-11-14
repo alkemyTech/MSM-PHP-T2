1c35c827ff06cf8944097a9000849939d0f1dfe2
alenmonti <amonti@fi.uba.ar>
2023-11-14 17:21:06 -0300

    AccountController + Route

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -10,9 +10,29 @@ class Account extends Model
     use HasFactory;
     protected $fillable = ['currency', 'transaction_limit', 'balance', 'user_id', 'cbu', 'deleted'];
     protected $hidden = ['user_id', 'created_at', 'updated_at', 'deleted'];
+    protected $with = ['user'];
 
     public function transactions()
     {
         return $this->hasMany(Transaction::class);
+    }
+
+    public function fixedTerms()
+    {
+        return $this->hasMany(FixedTerm::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function generateCbu()
+    {
+        $cbu = '';
+        for ($i = 0; $i < 22; $i++) {
+            $cbu .= rand(0, 9);
+        }
+        return $cbu;
     }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -11,4 +11,9 @@ class Role extends Model
     public $timestamps = true;
     protected $fillable = ['name', 'description'];
     protected $hidden = ['created_at', 'updated_at'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }

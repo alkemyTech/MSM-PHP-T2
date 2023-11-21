12749566ca2b62adde8eadc0d5ebd08ccc5a565a
GabiiMollo <jgabimollo@gmail.com>
2023-11-21 17:15:51 -0300

    solucion de role_id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,15 +17,17 @@ class UserController extends Controller
     public function deleteUser(Request $req, $id) {
         $authUser = Auth::user();
         $userToDelete = User::find($id);
+        $role_id_admin = Role::where('name', 'ADMIN')->value('id');
+        $role_id_user = Role::where('name', 'USER')->value('id');
         
             if (!$userToDelete) {
                 return response()->notFound(['message' => 'El usuario no existe']);
 
-            } elseif ($authUser->role_id == '1') {
+            } elseif ($authUser->role_id == $role_id_admin) {
                 $userToDelete->update(['deleted' => 1]);
-                return response()->ok(['message' => "Usuario '$userToDelete->id' eliminado correctamente", 200]);
+                return response()->ok(['message' => "Usuario '$userToDelete->name''$userToDelete->last_name' eliminado correctamente", 200]);
 
-            } elseif ($authUser->role_id == '2' && $authUser->id === $userToDelete->id) {
+            } elseif ($authUser->role_id == $role_id_user && $authUser->id === $userToDelete->id) {
                 $userToDelete->update(['deleted' => 1]);
                 return response()->ok(['message' => 'Ud. ha sido eliminado correctamente', 200]);
 
